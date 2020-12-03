d7b24b138d7fc47551d93f140973a393c29a1c0c
Surendra Kumar G <surendrakumar@autonom8.com>
2020-12-03 11:27:42 +0530

    user model

--- routes/apis/users.js
+++ routes/apis/users.js
@@ -1,4 +1,16 @@
 const express = require(`express`)
 const router = express.Router();
-router.get("/", (req, res) => { res.send("Users route") })
-module.exports = router;
+const { check, validationResult } = require('express-validator');
+router.post("/", [
+    check("name", `Name is required`).not().isEmpty(),
+    check("email", `Please enter valid email `).isEmail(),
+    check("password", `Please enter a password with 6 or more characters`).isLength({ min: 6 })
+], (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    console.log(`profile request body`, req.body);
+    res.send("Users route")
+})
+module.exports = router;  
--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require(`express`);
 const connectDB = require('./config/db');
 const app = express();
 const connectDb = require('./config/db')
+//Connection to DB
 connectDB();
+//Init middle ware 
+app.use(express.json({ extended: false }))
 const PORT = process.env.PORT || 5000
+//Defining routes
 app.use(`/api/post`, require("./routes/apis/posts"));
 app.use(`/api/user`, require("./routes/apis/users"));
 app.use(`/api/auth`, require("./routes/apis/auth"));
